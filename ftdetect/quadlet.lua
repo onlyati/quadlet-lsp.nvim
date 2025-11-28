04d081b97e98b64e42e217549264921feeb7dff3
onlyati <onlyati+github@proton.me>
2025-11-28 09:16:06 +0100

    add 'conf' as extension of dropins'

--- ftdetect/quadlet.lua
+++ ftdetect/quadlet.lua
@@ -1,13 +1,14 @@
 -- Register filetypes first
 vim.filetype.add({
-	extension = {
-		container = "quadlet",
-		volume = "quadlet",
-		network = "quadlet",
-		pod = "quadlet",
-		image = "quadlet",
-		build = "quadlet",
-		kube = "quadlet",
-		artifact = "quadlet",
-	},
+    extension = {
+        container = "quadlet",
+        volume = "quadlet",
+        network = "quadlet",
+        pod = "quadlet",
+        image = "quadlet",
+        build = "quadlet",
+        kube = "quadlet",
+        artifact = "quadlet",
+        conf = "quadlet",
+    },
 })
