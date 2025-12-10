a8125999000861dc08be7f37093e6699c514fb57
onlyati <onlyati+github@proton.me>
2025-12-10 22:23:25 +0100

    add root marker (#7)

--- lsp/quadlet.lua
+++ lsp/quadlet.lua
@@ -1,23 +1,28 @@
+---@type vim.lsp.Config
 return {
-	cmd = { "quadlet-lsp" },
-	filetypes = { "quadlet" },
+    cmd = { "quadlet-lsp" },
+    filetypes = { "quadlet" },
+    root_dir = function(bufnr, on_dir)
+        local root_dir = vim.fs.root(bufnr, { ".quadletrc.json", ".git" })
+        if root_dir ~= nil then
+            on_dir(root_dir)
+        else
+            on_dir(vim.loop.cwd())
+        end
+    end,
 
-	root_dir = function(_, on_dir)
-		on_dir(vim.fn.getcwd())
-	end,
+    handlers = {
+        ["window/showMessage"] = function(_, result, ctx)
+            local client = vim.lsp.get_client_by_id(ctx.client_id)
+            local client_name = (client and client.name) or "quadlet-lsp.nvim"
+            local level = ({
+                [vim.lsp.protocol.MessageType.Error] = vim.log.levels.ERROR,
+                [vim.lsp.protocol.MessageType.Warning] = vim.log.levels.WARN,
+                [vim.lsp.protocol.MessageType.Info] = vim.log.levels.INFO,
+                [vim.lsp.protocol.MessageType.Log] = vim.log.levels.DEBUG,
+            })[result.type] or vim.log.levels.INFO
 
-	handlers = {
-		["window/showMessage"] = function(_, result, ctx)
-			local client = vim.lsp.get_client_by_id(ctx.client_id)
-			local client_name = (client and client.name) or "quadlet-lsp.nvim"
-			local level = ({
-				[vim.lsp.protocol.MessageType.Error] = vim.log.levels.ERROR,
-				[vim.lsp.protocol.MessageType.Warning] = vim.log.levels.WARN,
-				[vim.lsp.protocol.MessageType.Info] = vim.log.levels.INFO,
-				[vim.lsp.protocol.MessageType.Log] = vim.log.levels.DEBUG,
-			})[result.type] or vim.log.levels.INFO
-
-			vim.notify(string.format("[%s] %s", client_name, result.message), level)
-		end,
-	},
+            vim.notify(string.format("[%s] %s", client_name, result.message), level)
+        end,
+    },
 }
